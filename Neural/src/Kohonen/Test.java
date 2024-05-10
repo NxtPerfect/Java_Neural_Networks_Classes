248ae9ca08a47d01eba44987c5b9d437c22c4f09
NxtPerfect <alakaxan@protonmail.com>
2024-05-10 09:46:10 +0200

    Naprawiono błąd niedostosywania się siatki do drugiego obrazka
    ostateczne ustawienie interfejsu
    zmniejszenie rozciągania wertykalnego

--- Neural/src/Kohonen/Test.java
+++ Neural/src/Kohonen/Test.java
@@ -3,7 +3,9 @@ package Kohonen;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
@@ -34,7 +36,7 @@ public class Test extends JFrame {
 	private MyComponent komponent;
 	private final String sciezkaKolo = "src/Kohonen/kolo.png", sciezkaTrojkat = "src/Kohonen/trojkat.png",
 			sciezkaKwadrat = "src/Kohonen/kwadrat.png";
-	private BufferedImage obraz1, obraz2;
+	private BufferedImage obraz1, obraz2, obrazek;
 	private JLabel img1, img2;
 	private JRadioButton radioWTA, radioWTM;
 	private SOM som;
@@ -42,9 +44,9 @@ public class Test extends JFrame {
 	private JComboBox<String> comboPierwszy = new JComboBox<String>(figures);
 	private JComboBox<String> comboDrugi = new JComboBox<String>(figures);
 	private final int WIERSZE = 10, KOLUMNY = 10;
-	private final double AETA = 0.01, AEPSETA = 0.99, AEPSS = 0.9999;
+	private final double AETA = 0.1, AEPSETA = 0.9999, AEPSS = 0.999;
 	private final int WYMIARY_OBRAZKA = 250;
-	private int iteracja = 0, MAKSYMALNA_ITERACJA = 5000;
+	private int iteracja = 0, MAKSYMALNA_ITERACJA = 200;
 
 	private class MyComponent extends JComponent {
 		@Override
@@ -55,28 +57,28 @@ public class Test extends JFrame {
 			Random rand = new Random();
 			int a = rand.nextInt(WYMIARY_OBRAZKA);
 			int b = rand.nextInt(WYMIARY_OBRAZKA);
-			int ktory = rand.nextInt(2);
-			BufferedImage obrazek = obraz1;
 
-			System.out.println(iteracja + " " + Math.round(MAKSYMALNA_ITERACJA / 2));
+//			System.out.println(iteracja + " " + Math.round(MAKSYMALNA_ITERACJA / 2));
 			// Jeśli wybrany obrazek to prawy
 			if (iteracja == Math.round(MAKSYMALNA_ITERACJA / 2)) {
+				System.out.println("Obraz2");
 				obrazek = obraz2;
 				som.eta = AETA;
-				som.s = AEPSS;
+//				som.epsEta = AEPSETA;
+//				som.epsS = AEPSS;
 				img1.setBorder(null);
 				img2.setBorder(BorderFactory.createLineBorder(Color.RED));
 			}
-			if (iteracja > MAKSYMALNA_ITERACJA) {
+			if (iteracja >= MAKSYMALNA_ITERACJA || iteracja == 0) {
+				System.out.println("Obraz1");
+				obrazek = obraz1;
 				iteracja = 0;
 				som.eta = AETA;
-				som.s = AEPSS;
+//				som.epsEta = AEPSETA;
+//				som.epsS = AEPSS;
 				img1.setBorder(BorderFactory.createLineBorder(Color.RED));
 				img2.setBorder(null);
 			}
-//			if (ktory == 1) {
-//				obrazek = obraz2;
-//			}
 			if (obrazek == null) {
 				try {
 					obrazek = ImageIO.read(new File(sciezkaKolo));
@@ -91,20 +93,23 @@ public class Test extends JFrame {
 			}
 
 			// Oblicz wektor wejścia
-			double x = (-1 * (w / 2.0 - a * 2) * 2 / w - 0.5) * 2 / 3;
-			double y = (-1 * (h / 2.0 - b * 2) * 2 / h) * 2 / 3;
+			double x = (-1 * (w / 2.0 - a * 2) * 2 / w) * 2 / 3;
+			double y = (-1 * (h / 2.0 - b * 2) * 2 / h - 1.0) * 2 / 3;
 			Vec2D wejscia = new Vec2D(x, y);
 			// WTA
 			if (radioWTA.isSelected()) {
 				som.uczWTA(wejscia);
-				super.paintComponent(g);
 				iteracja++;
+				System.out.println(iteracja);
+				super.paintComponent(g);
 				return;
 			}
 			// WTM
 			som.ucz(wejscia);
-			super.paintComponent(g);
 			iteracja++;
+			System.out.println(iteracja);
+			super.paintComponent(g);
+			return;
 		}
 	}
 
@@ -115,17 +120,19 @@ public class Test extends JFrame {
 		setVisible(true);
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		setBounds(d.width / 4, d.height / 4, d.width / 2, d.height / 2);
-		setLayout(new GridLayout(1, 2, 2, 2));
-		gridLewo = new JPanel(new GridLayout(3, 1, 10, 10));
-		gridPrawo = new JPanel(new GridLayout(3, 1, 10, 10));
+		setLayout(new GridLayout(1, 2, 20, 20));
+		gridLewo = new JPanel(new GridLayout(3, 1, 0, 0));
+		gridPrawo = new JPanel(new GridLayout(3, 1, 40, 40));
 		add(gridLewo);
 		add(gridPrawo);
 
+		Font newFont = new Font("Serif", Font.PLAIN, 20);
+
 		img1 = new JLabel();
-		img1.setBackground(Color.gray);
+		img1.setBackground(Color.DARK_GRAY);
 		img1.setBorder(BorderFactory.createLineBorder(Color.RED));
 		img2 = new JLabel();
-		img2.setBackground(Color.gray);
+		img2.setBackground(Color.DARK_GRAY);
 
 		// Inicjalizacja obrazków i ustawienie obrazków combo boxem
 		obraz1 = ustawObraz(obraz1, sciezkaKolo, img1);
@@ -180,7 +187,9 @@ public class Test extends JFrame {
 		});
 
 		radioWTA = new JRadioButton("WTA");
+		radioWTA.setFont(newFont);
 		radioWTM = new JRadioButton("WTM");
+		radioWTM.setFont(newFont);
 		radioWTM.setSelected(true);
 
 		komponent = new MyComponent();
@@ -188,12 +197,15 @@ public class Test extends JFrame {
 		gridLewo.add(komponent);
 		gridLewo.add(img2);
 
+		comboPierwszy.setFont(newFont);
 		gridPrawo.add(comboPierwszy);
+		comboDrugi.setFont(newFont);
 		gridPrawo.add(comboDrugi);
 		JButton trenuj = new JButton("Trenuj");
+		trenuj.setFont(newFont);
 		som = new SOM(WIERSZE, KOLUMNY, AETA, AEPSETA, AEPSS);
 		// Timer ustaw czas
-		timer = new Timer(0, new ActionListener() {
+		timer = new Timer(20, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				komponent.repaint();
